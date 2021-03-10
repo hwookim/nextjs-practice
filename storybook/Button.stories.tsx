3cc7bc70f9ab9bdb5f66b54657819a99e6d888f3
hwookim <gusdn9754@gmail.com>
2021-03-10 11:41:11 +0900

    refactor: 프로덕션 코드 src 폴더로 패키징

--- storybook/Button.stories.tsx
+++ storybook/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Button from '../components/Button';
+import Button from '../src/components/Button';
 
 storiesOf('Button', module).add('with text', () => {
   return <Button content="click" />;
