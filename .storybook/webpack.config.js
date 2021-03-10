36d1282cb47441c1411c9f8f9e2d52c70a3b6833
hwookim <gusdn9754@gmail.com>
2021-03-10 11:46:49 +0900

    chore: alias 적용

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
@@ -6,6 +8,11 @@ module.exports = ({ config }) => {
       presets: [require.resolve('babel-preset-react-app')],
     },
   });
+  config.resolve.alias = {
+    ...config.resolve.alias,
+    '@': path.resolve(__dirname, '../src'),
+    '@components': path.resolve(__dirname, '../src/components'),
+  };
 
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
--- storybook/Button.stories.tsx
+++ storybook/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import Button from '../src/components/Button';
+import Button from '@/components/Button';
 
 storiesOf('Button', module).add('with text', () => {
   return <Button content="click" />;
