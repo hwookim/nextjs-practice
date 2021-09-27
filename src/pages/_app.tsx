40d719944d8436f39b00ae2cc204e9cfc155eece
hwookim <gusdn9754@gmail.com>
2021-09-28 03:29:35 +0900

    feat: Head 컴포넌트를 통한 title 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>next-practice</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
