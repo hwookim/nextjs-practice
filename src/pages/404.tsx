8a0e9289ff131c2b7aac556ef717c31faa26a5df
hwookim <gusdn9754@gmail.com>
2021-10-01 19:25:12 +0900

    feat: Redirect on 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function NotFound(): JSX.Element {
+  const router = useRouter();
+  useEffect(() => {
+    router.replace('/');
+  }, []);
+
   return <div>404</div>;
 }
